6ebf9e19aa5eb8fdb8ec2df46413fa987f4b8e17
1039874238 <50441575+1039874238@users.noreply.github.com>
2022-01-26 10:44:47 +0800

    Add files via upload
    add

---
+++ src/libs/LoveMsg/joke.ts
@@ -0,0 +1,27 @@
+/**
+ * @name joke
+ * @description 说笑话
+ */
+import API from '../../api/loveMsg'
+import { wxNotify } from '../WxNotify'
+
+export const joke = async() => {
+  const res = await API.getJoke()
+
+  let text = '小宝贝:\n'
+
+  text += `
+ 给你说两个雷人的笑话吧😝\n`
+
+  text += `
+ ${[res[0]].map(n => `『${n.title}』${n.content}`).join('\n\n')}`
+
+  const template = {
+    msgtype: 'text',
+    text: {
+      content: text,
+    },
+  }
+
+  await wxNotify(template)
+}

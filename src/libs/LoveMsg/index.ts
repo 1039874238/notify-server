3c51504cdbab83a821670e27ddafcc2e95e76b8f
1039874238 <50441575+1039874238@users.noreply.github.com>
2022-01-26 10:45:28 +0800

    Update index.ts
    edit

--- src/libs/LoveMsg/index.ts
+++ src/libs/LoveMsg/index.ts
@@ -3,24 +3,27 @@
  * @description 入口
  */
 import dotenv from 'dotenv'
+import dayjs from 'dayjs'
 import { goodMorning } from './goodMorning'
 import { goodAfternoon } from './goodAfternoon'
 import { goodEvening } from './goodEvening'
+import { joke } from './joke'
 dotenv.config()
 
-const { MESSAGE_TYPE } = process.env
-
 export default function main() {
-  if (MESSAGE_TYPE === 'goodAfternoon') {
-    // 午安
-    goodAfternoon()
-  }
-  else if (MESSAGE_TYPE === 'goodEvening') {
-    // 晚安
-    goodEvening()
-  }
-  else {
-    // 早安
+  const nowTime = dayjs().hour()
+  if (nowTime >= 7 && nowTime < 9)
     goodMorning()
-  }
+
+  else if (nowTime >= 11 && nowTime < 13)
+    goodAfternoon()
+
+  else if (nowTime >= 22 && nowTime < 23)
+    goodEvening()
+
+  else
+  if (nowTime >= 9 && nowTime <= 18)
+    joke()
+  else
+    console.log('当前时间不发消息')
 }
